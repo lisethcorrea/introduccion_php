7c0a42f85b70adb0242f654fab3714a256e4e19d
dannyardila <dannyardila059@gmail.com>
2023-07-17 17:35:25 -0500

    comentarios en php

---
+++ 1_comentarios.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!--formas de incluir codigo php-->
+
+    <?php
+    //comentario en una linea
+    /*Esto es un comentario de varias lineas*/
+
+
+    ?>
+
+    <!--
+        codigo php
+
+   </script language="php">
+
+</scrpt>-->
+
+        
+
+        
+</body>
+</html>
